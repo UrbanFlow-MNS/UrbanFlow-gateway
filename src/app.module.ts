a08076dbe44dccce73977c632c33fcca76ddc1c6
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-03-29 11:35:24 +0200

    feat: connection logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,41 +7,59 @@ import { AppService } from './app.service';
 import { AuthController } from './controllers/auth.controller';
 import { PrometheusController } from './controllers/prometheus.controller';
 import { UserController } from './controllers/user.controller';
+import { LogsController } from './controllers/logs.controller';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { PrometheusService } from './services/prometheus.service';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
-        JwtModule.register({
-            global: true,
-            secret: process.env.JWT_SECRET,
-        }),
-        ClientsModule.register([
-            {
-                name: 'AUTH_SERVICE',
-                transport: Transport.TCP,
-                options: {
-                    host: process.env.AUTH_SERVICE_HOST || 'localhost',
-                    port: Number.parseInt(process.env.AUTH_SERVICE_PORT || '4001'),
-                },
-            },
-            {
-                name: 'USER_SERVICE',
-                transport: Transport.TCP,
-                options: {
-                    host: process.env.USER_SERVICE_HOST || 'localhost',
-                    port: Number.parseInt(process.env.USER_SERVICE_PORT || '4001'),
-                },
-            }
-        ]),
-    ],
-    controllers: [AppController, AuthController, UserController, PrometheusController],
-    providers: [
-        AppService,
-        JwtAuthGuard,
-        PrometheusService,
-        { provide: 'IPrometheusService', useClass: PrometheusService },
-    ],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+    }),
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: process.env.AUTH_SERVICE_HOST || 'localhost',
+          port: Number.parseInt(process.env.AUTH_SERVICE_PORT || '4001'),
+        },
+      },
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: process.env.USER_SERVICE_HOST || 'localhost',
+          port: Number.parseInt(process.env.USER_SERVICE_PORT || '4001'),
+        },
+      },
+      {
+        name: 'LOGS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBIT_MQ || 'amqp://localhost:5672'],
+          queue: 'LOGS_QUEUE',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [
+    AppController,
+    AuthController,
+    UserController,
+    PrometheusController,
+    LogsController,
+  ],
+  providers: [
+    AppService,
+    JwtAuthGuard,
+    PrometheusService,
+    { provide: 'IPrometheusService', useClass: PrometheusService },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
