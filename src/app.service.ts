4ad4613dfeddd038df0cdf04a28be9d776ee492b
Theo Sementa <theosementa@gmail.com>
2026-04-07 10:16:00 +0200

    feat: update package

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! From CI/CD again again ?';
+    return 'Hello World! From CI/CD again again ? Ok now its good';
   }
 }
