2bfc14b5223a39cb33eff65a637197c92336a07f
Theo Sementa <theosementa@gmail.com>
2026-06-17 08:53:11 +0200

    feat: update CORS

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
             'http://localhost:5173',
             'http://localhost:5174',
             'https://urbanflow.lazyy.fr',
+            'https://auth.urbanflow.lazyy.fr',
         ].filter(Boolean),
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
         credentials: true,
