2d0b3b7518fd26608c13570480c4d8f344ff34a7
Theo Sementa <theosementa@gmail.com>
2026-07-05 09:30:21 +0200

    feat: link auth and user with gRPC

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AuthController } from './auth.controller';
+
+@Module({
+    imports: [
+        ClientsModule.register([
+            {
+                name: 'AUTH_SERVICE',
+                transport: Transport.TCP,
+                options: {
+                    host: process.env.AUTH_SERVICE_HOST || 'localhost',
+                    port: Number.parseInt(process.env.AUTH_SERVICE_PORT || '4001')
+                }
+            }
+        ])
+    ],
+    controllers: [AuthController]
+})
+
+export class AuthModule { }
