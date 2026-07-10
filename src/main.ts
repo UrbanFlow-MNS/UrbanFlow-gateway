e5d7219e791315ba234ba48a5d2105df55107c04
Theo Sementa <theosementa@gmail.com>
2026-07-10 07:28:35 +0200

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { GlobalGatewayExceptionFilter } from './exception.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.use(helmet());
     app.getHttpAdapter().getInstance().set('trust proxy', 1);
 
     app.enableCors({
