72cebacaa8414721946a6adaa5496dac427ab53b
Logan <logan.pallara@gmail.com>
2026-08-02 17:43:25 +0200

    feat: forward caller id to incidents service

--- src/controllers/incident.controller.ts
+++ src/controllers/incident.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, UseGuards } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { Observable } from "rxjs";
-import { JwtAuthGuard } from "../guards/jwt.guard";
+import { CurrentUser } from "../decorators/current-user.decorator";
+import { JwtAuthGuard, JwtPayload } from "../guards/jwt.guard";
 
 @Controller('incidents')
 export class IncidentController {
@@ -9,8 +10,8 @@ export class IncidentController {
 
     @UseGuards(JwtAuthGuard)
     @Post()
-    create(@Body() body: any): Observable<any> {
-        return this.incidentsClient.send({ cmd: 'incident.create' }, body);
+    create(@Body() body: any, @CurrentUser() user: JwtPayload): Observable<any> {
+        return this.incidentsClient.send({ cmd: 'incident.create' }, { ...body, callerId: user.sub });
     }
 
     @Get()
@@ -25,13 +26,13 @@ export class IncidentController {
 
     @UseGuards(JwtAuthGuard)
     @Patch(':id')
-    update(@Param('id', ParseIntPipe) id: number, @Body() body: any): Observable<any> {
-        return this.incidentsClient.send({ cmd: 'incident.update' }, { id, dto: body });
+    update(@Param('id', ParseIntPipe) id: number, @Body() body: any, @CurrentUser() user: JwtPayload): Observable<any> {
+        return this.incidentsClient.send({ cmd: 'incident.update' }, { id, dto: body, callerId: user.sub, callerRole: user.role });
     }
 
     @UseGuards(JwtAuthGuard)
     @Delete(':id')
-    remove(@Param('id', ParseIntPipe) id: number): Observable<any> {
-        return this.incidentsClient.send({ cmd: 'incident.remove' }, id);
+    remove(@Param('id', ParseIntPipe) id: number, @CurrentUser() user: JwtPayload): Observable<any> {
+        return this.incidentsClient.send({ cmd: 'incident.remove' }, { id, callerId: user.sub, callerRole: user.role });
     }
 }
